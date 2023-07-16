125569c6fe46f27e24bc978e72870bf716277075
Quajak <jasper.gerigk@mail.utoronto.ca>
2023-07-16 16:39:40 -0400

    Further code cleanup

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -14,29 +14,14 @@ Office.onReady(() => {
  * @param event
  */
 function action(event: Office.AddinCommands.Event) {
-  const message: Office.NotificationMessageDetails = {
-    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-    message: "Performed action." + Office.context.mailbox.item.subject,
-    icon: "Icon.80x80",
-    persistent: true,
-  };
-
   const open_message = Office.context.mailbox.item;
 
-  Office.context.mailbox.item.body.getAsync(
-    "text",
-    { asyncContext: "This is passed to the callback" },
-    function callback(result) {
-      // Do something with the result.
-      Office.context.mailbox.displayNewAppointmentForm({
-        subject: "Event: " + open_message.subject,
-        body: result.value,
-      } as Office.AppointmentForm);
-    }
-  );
-
-  // Show a notification message
-  Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
+  Office.context.mailbox.item.body.getAsync("text", {}, function callback(result) {
+    Office.context.mailbox.displayNewAppointmentForm({
+      subject: "Event: " + open_message.subject,
+      body: result.value,
+    } as Office.AppointmentForm);
+  });
 
   // Be sure to indicate when the add-in command function is complete
   event.completed();
